1132db3746c05cdcf4e8214cf3985205385f76c8
Kseniya <bks2607@yandex.ru>
2025-10-30 14:59:12 +0300

    remove useless files

--- lab_python_fp/sort/sort.py
+++
@@ -1,7 +0,0 @@
-def sort_without_lambda(data):
-    return sorted(data, key=abs, reverse=True)
-
-def sort_with_lambda(data):
-    return sorted(data, key=lambda x: abs(x), reverse=True)
-
-data = [4, -30, 30, 100, -100, 123, 1, 0, -1, -4]
